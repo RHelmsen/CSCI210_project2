341012c2cd6700b37b4788969005a672901f3d6b
RHelmsen <82037964+RHelmsen@users.noreply.github.com>
2024-10-27 21:47:39 -0600

    Update mkdir_splitpath.c

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -1,6 +1,7 @@
 
 
 #include "types.h"
+#include "string.h"
 
 extern struct NODE* root;
 extern struct NODE* cwd;
@@ -8,11 +9,52 @@ extern struct NODE* cwd;
 //make directory
 void mkdir(char pathName[]){
 
+    int exists=0;
+    char baseName;
+    char dirName;
+    if(pathName == "/"){
+        printf(" % s\n","MKDIR ERROR: no path provided");
+        return;
+    }
     // TO BE IMPLEMENTED
     //
     // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+    struct NODE* parent;
+    struct NODE* currNode;
+    struct NODE* prevNode;
+    struct NODE* newNode;
+    struct NODE n;
+    parent = splitPath(pathName, baseName, dirName);
+    if(parent==NULL){
+        return;
+    }
+    currNode = parent->childPtr;
+    prevNode = parent;
+    while(currNode!=NULL){
+        if(currNode->name==baseName){
+            exists=1;
+            break;
+        }
+        prevNode=currNode;
+        currNode=currNode->siblingPtr;
+    } 
 
-    printf("TO BE IMPLEMENTED\n");
+
+    if(exists == 0){
+        n.parentPtr=parent;
+        *n.name=baseName;
+        n.fileType="D";
+        if(prevNode==parent){
+            parent->childPtr=&n;
+        }
+        else{
+            prevNode->siblingPtr=&n;
+        }
+        printf(" % s\n",strcat(strcat("MKDIR SUCCESS: node ",pathName)," created"));
+    }
+    else{
+        printf(" % s\n",strcat(strcat("MKDIR ERROR: directory ",baseName)," already exists"));
+    }
 
     return;
 }
@@ -26,6 +68,42 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
     // SEE THE PROVIDED EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
 
     // YOUR CODE HERE
-    //
-    return NULL;
+    int pathSize = sizeof(pathName)/sizeof(pathName[0]);
+    char *pathName2;
+    strcpy(pathName2, pathName);
+    char *token = strtok(pathName2, '/');
+    while(strstr(pathName2, '/')){
+        token = strtok(NULL,'/');
+    }
+    baseName=token;
+    int baseSize = sizeof(baseName)/sizeof(baseName[0]);
+    strncpy(dirName,pathName,pathSize-baseSize);
+
+    char *dirName2;
+    char* directory = strtok(dirName2,'/');
+    struct NODE* currNode;
+    if(pathName[0]=='/'){
+        currNode = root;
+    }
+    else{
+        currNode = cwd;
+        directory=strtok(NULL,'/');
+    }
+    while(directory != NULL){
+        currNode = currNode->childPtr;
+        while(currNode!=NULL){
+            if(currNode->name==directory){
+                break;
+            }
+            currNode=currNode->siblingPtr;
+        } 
+        if(currNode==NULL){
+            printf(" % s\n",strcat(strcat("ERROR: directory ",directory)," does not exist"));
+            return NULL;
+        }
+
+        directory=strtok(NULL,'/');
+    }
+    return currNode;
+
 }
